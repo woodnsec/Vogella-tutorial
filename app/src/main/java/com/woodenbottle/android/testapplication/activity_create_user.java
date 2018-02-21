45741a5f4cd33411ea2b5e1a0aff12c1bcf85b56
nwood <nwood01@unomaha.edu>
2018-02-20 18:37:18 -0600

    radio buttons working and implemented in toast

--- app/src/main/java/com/woodenbottle/android/testapplication/activity_create_user.java
+++ app/src/main/java/com/woodenbottle/android/testapplication/activity_create_user.java
@@ -1,22 +1,48 @@
 package com.woodenbottle.android.testapplication;
 
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class activity_create_user extends AppCompatActivity {
+    boolean male = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_user);
+        final RadioGroup radioGroup = (RadioGroup) findViewById(R.id.gender);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.male:
+                        male = true;
+                        break;
+                    case R.id.female:
+                        male = false;
+                        break;
+                }
+            }
+        });
     }
 
+
+
     public void onClick(View view) {
+        String gender = "male";
+
         EditText input = (EditText) findViewById(R.id.username);
         String string = input.getText().toString();
-        Toast.makeText(this, "User " + string + " created", Toast.LENGTH_LONG).show();
+        if (male == false) {
+            gender = "female";
+        }
+
+
+        Toast.makeText(this, "gender=" + gender + " User " + string + " created", Toast.LENGTH_LONG).show();
     }
 }
